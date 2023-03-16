92a67a431ded1218b7a672581d9d017d47723d98
ameko <ameko.dev@gmail.com>
2023-03-16 17:35:41 +0800

    no message

--- lib/data/repositories/ethereum_insurance_contract/ethereum_insurance_contract_repo.dart
+++ lib/data/repositories/ethereum_insurance_contract/ethereum_insurance_contract_repo.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
 import 'package:ftec5520_client/domain/entities/insurance_contract.dart';
 import 'package:ftec5520_client/domain/entities/contract_template.dart';
 import 'package:ftec5520_client/domain/interfaces/repositories/insurance_contract_repository.dart';
+import 'package:web3dart/web3dart.dart';
 
 class EthereumInsuranceContractRepo implements InsuranceContractRepository {
-  final String url = '';
+  final String url = "https://goerli.infura.io/v3/YOUR_INFURA_PROJECT_ID";
   final String account = '';
+
+  var httpClient = HttpClient();
+  final Web3Client web3client = Web3Client(url, httpClient);
 
   @override
   Future<List<InsuranceContract>> getPurchasedInsuranceContracts() async {

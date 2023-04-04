32c2bbb98b6a0bfa5e4042be5032f0066a1bae02
ameko <ameko.dev@gmail.com>
2023-04-04 15:21:13 +0800

    no message

--- lib/app/home/home.dart
+++ lib/app/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ftec5520_client/app/utils/notification_utils.dart';
 import 'package:ftec5520_client/app/wallet/wallet.dart';
 
 import '../contract/purchased_contract_list.dart';
@@ -12,16 +13,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   final GlobalKey<WalletState> _walletKey = GlobalKey<WalletState>();
   final GlobalKey<PurchasedContractListState> _purchasedContractListKey =
       GlobalKey<PurchasedContractListState>();
 
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   @override
   void initState() {
-    initNotifications();
     super.initState();
   }
 
@@ -29,24 +29,8 @@ class _HomeState extends State<Home> {
     print('refresh');
     _walletKey.currentState!.getWalletData();
     _purchasedContractListKey.currentState!.getPurchasedContracts();
-    _showNotification("title", "body");
-  }
-
-  Future<void> initNotifications() async {
-    // Android notification settings
-    const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('notification.png');
-
-    // iOS notification settings
-    const DarwinInitializationSettings initializationSettingsDarwin = DarwinInitializationSettings();
-
-    // General initialization settings
-    const InitializationSettings initializationSettings = InitializationSettings(
-      iOS: initializationSettingsDarwin,
-      android: initializationSettingsAndroid,
-    );
-
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    NotificationUtils.showNotification(
+        title: "title", body: "body", payload: "payload", context: context);
   }
 
   // void onDidReceiveLocalNotification(
@@ -54,24 +38,6 @@ class _HomeState extends State<Home> {
   //   // display a dialog with the notification details, tap ok to go to another page
   //   showNotification("title", "body");
   // }
-
-  Future<void> _showNotification(String title, String body) async {
-
-    const DarwinNotificationDetails darwinNotificationDetails = DarwinNotificationDetails(
-      presentAlert: true,
-      presentBadge: true,
-      presentSound: true,
-    );
-
-    final NotificationDetails notificationDetails = NotificationDetails(iOS: darwinNotificationDetails);
-
-    await flutterLocalNotificationsPlugin.show(
-      0, // Notification ID
-      title,
-      body,
-      notificationDetails,
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
